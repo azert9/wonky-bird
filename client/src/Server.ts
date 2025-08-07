4bdf54662d839f38339b3b4e242fe2ba18306289
azert9 <no.mail@jloc.fr>
2025-08-08 00:39:01 +0200

    persist queued score to localstorage until they are uploaded

--- client/src/Server.ts
+++ client/src/Server.ts
@@ -1,13 +1,23 @@
 export default class {
     private readonly __username: string;
-    private readonly __queue: number[] = [];
+    private readonly __score_queue: number[] = [];
 
     constructor(username: string) {
         this.__username = username;
+
+        const savedScoreQueue = window.localStorage.getItem("score-queue");
+        if (savedScoreQueue !== null) {
+           try {
+               this.__score_queue = JSON.parse(savedScoreQueue);
+           } catch (e) {
+               console.error(`failed to parse saved score queue: ${e}`);
+           }
+        }
     }
 
     async push_score(score: number) {
-        this.__queue.push(score);
+        this.__score_queue.push(score);
+        this.__save_score_queue();
         await this.__try_upload_scores();
     }
 
@@ -17,21 +27,26 @@ export default class {
     }
 
     private async __try_upload_scores() {
-        while (this.__queue.length !== 0) {
+        while (this.__score_queue.length !== 0) {
             // TODO: post multiple scores at once
             // TODO: avoid posting duplicate
             const resp = await fetch("api/score", {
                 method: "POST",
                 body: JSON.stringify({
                     username: this.__username,
-                    score: this.__queue[0],
+                    score: this.__score_queue[0],
                 }),
             });
             if (!resp.ok) {
                 throw new Error(`failed to post score: ${resp.status}`);
             }
-            this.__queue.shift();
+            this.__score_queue.shift();
+            this.__save_score_queue();
         }
+    }
+
+    private __save_score_queue() {
+        window.localStorage.setItem("score-queue", JSON.stringify(this.__score_queue));
     }
 }
 
